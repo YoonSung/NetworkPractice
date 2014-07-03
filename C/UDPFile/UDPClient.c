eaaff0012b5e70bf0a08f2cc7f7f870042da2e56
Yoonsung <lvev9925@naver.com>
2014-07-03 13:50:36 +0900

    Add Comment, Remove Obsolete Files

--- C/UDPFile/UDPClient.c
+++ C/UDPFile/UDPClient.c
@@ -5,10 +5,10 @@
 #include<netinet/in.h>
 #include <errno.h>
 
-#define TIME_SERVER "127.0.0.1"
-#define TIME_PORT 8080
-#define HEAD_BUFFER_SIZE 8
-#define BODY_BUFFER_SIZE 1024
+#define SERVER_IP 				"127.0.0.1"
+#define SERVER_PORT 			8080
+#define HEAD_BUFFER_SIZE 	8
+#define BODY_BUFFER_SIZE 	1024
 
 void error(char* msg) {
 	fprintf(stderr, "ERROR: %s. (errno = %d)\n", msg, errno);
@@ -18,22 +18,23 @@ void error(char* msg) {
 int main(int argc, char *argv[]) 
 {
 	//네트워크로 전송할 총 데이터의 길이
-	//const int BUFFER_SIZE = HEAD_BUFFER_SIZE + BODY_BUFFER_SIZE;
-	int BUFFER_SIZE = BODY_BUFFER_SIZE;
+	const int BUFFER_SIZE = HEAD_BUFFER_SIZE + BODY_BUFFER_SIZE;
+	printf("BUFFER SIZE : %d\n", BUFFER_SIZE);
+	//int BUFFER_SIZE = HEAD_BUFFER_SIZE + BODY_BUFFER_SIZE;
 	
 	/* 변수 선언 */
 	int sock;	//생성된 소켓번호(socket descriptor)를 저장할 변수
 	struct sockaddr_in server, client; //서버 주소정보를 담을 변수들
+	int serveraddr_len;
 	char head_buf[HEAD_BUFFER_SIZE];
 	char body_buf[BODY_BUFFER_SIZE];
-	char recv_buf[HEAD_BUFFER_SIZE];
-	char buf[BUFFER_SIZE]; //송. 수신데이터를 담을 버퍼변수
-	int buf_len; //전송, 수신데이터 크기를 담을 변수
+	char ack_buf[HEAD_BUFFER_SIZE];//acknoledgement를 받기위한 buffer
+	char buf[BUFFER_SIZE]; //송. 수신데이터를 담을 버퍼변수 (head_buf + body_buf)
 	
 	/* 소켓 주소정보를 담고있는 sockaddr_in sturcture에 목표 호스트 서버에 대한 정보를 저장한다. */
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr(TIME_SERVER);
-	server.sin_port = htons(TIME_PORT);
+	server.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server.sin_port = htons(SERVER_PORT);
 	
 	/* 데이터 전송에 사용되는 소켓을 생성하고, 생성된 소켓번호 descriptor(각 소켓간의 구분자)를 가져온다.*/
 	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1 ) 
@@ -60,8 +61,9 @@ int main(int argc, char *argv[])
 	}
 	
 	
-	/* 클라이언트에서 처리해야하는 일을 수행 */
+	/* 클라이언트에서 처리해야하는 일을 수행하는 Business Logic부분 */
 	char* file_name = "reading01.pdf";
+	//char* file_name = "/Users/next/Desktop/test.txt";
 	FILE* file = fopen(file_name, "rb");
 
 	if (file == NULL) {
@@ -72,70 +74,66 @@ int main(int argc, char *argv[])
 		bzero(buf, BUFFER_SIZE); //byte stream을 0으로 채운다.
 		bzero(head_buf, HEAD_BUFFER_SIZE); //byte stream을 0으로 채운다.
 		bzero(body_buf, BODY_BUFFER_SIZE); //byte stream을 0으로 채운다.
-		
+		bzero(ack_buf, HEAD_BUFFER_SIZE);
+				
 		int fs_block_sz;
 		int send_data_sz;
-		int ack = 2;
+		int current_ack = 0, recv_ack = 0;
+		serveraddr_len = sizeof(server);
 		
-		//while((fs_block_sz = fread(body_buf, sizeof(char), BODY_BUFFER_SIZE, file)) > 0)
-		while((fs_block_sz = fread(buf, sizeof(char), BUFFER_SIZE, file)) > 0)
+
+		while((fs_block_sz = fread(body_buf, sizeof(char), BODY_BUFFER_SIZE, file)) > 0)
 		{
-			//printf("fs_block_sz : %d\n", fs_block_sz);
-			/*
-			sprintf(head_buf, "%d", ack);
+			//head buffer에 현재 데이터의 번호를 저장 (ack저장)
+			sprintf(head_buf, "%d", current_ack);
+			
+			//entire buffer에 헤더버퍼, body버퍼를 더한다.
 			memcpy(buf, head_buf, (HEAD_BUFFER_SIZE) * sizeof(char));
 			memcpy(buf+HEAD_BUFFER_SIZE, body_buf, (BODY_BUFFER_SIZE) * sizeof(char));
-			
-			for (int i = 0 ; i < BUFFER_SIZE ; ++i)
-			{
-					printf("%d : %c\n", i, buf[i]);
-			}
+
 SEND_DATA:
-			*/
+			//전체데이터 전송
+			send_data_sz = sendto(sock, buf, fs_block_sz+HEAD_BUFFER_SIZE, 0, (struct sockaddr *)&server, sizeof(server));
+			//printf("send_data_sz : %d\n", send_data_sz); //test
 			
-			send_data_sz = sendto(sock, buf, fs_block_sz, 0, (struct sockaddr *)&server, sizeof(server));
-			printf("send_data_sz : %d\n", send_data_sz);
-			//buf_len = recvfrom(sock, buf, 256, 0, (struct sockaddr *)0, (int *)0); //연결형은 recv, 비연결형은 recvfrom을 사용
-			
+			//데이터 전송에 실패할 경우
 			if (send_data_sz < 0)
 			{
-				error("Faile To Send File");
+				error("Fail To Send File");
 			}
 			
-			if (send_data_sz < fs_block_sz) 
+			//연결형은 recv, 비연결형은 recvfrom을 사용
+			recvfrom(sock, ack_buf, HEAD_BUFFER_SIZE, 0, (struct sockaddr *)&server, &serveraddr_len);
+			
+			//헤더버퍼에서 문자열로 이루어진 숫자를 integer로 변환
+			recv_ack = atoi(ack_buf);
+			//printf("current_ack : %d\n", current_ack);//test
+			//printf("recv_ack : %d\n", recv_ack);//test
+			
+			//실제로 수신자에게 보낸데이터 크기와, 파일로부터 읽은데이터 크기가 다를경우, 재전송한다.
+			if (send_data_sz-HEAD_BUFFER_SIZE < fs_block_sz) 
 			{
-				//goto SEND_DATA;	
+				goto SEND_DATA;	
 			}
+			
+			//수신자에게 전달한 데이터의 수신이 성공적인것을 acknoledgement를 통해 확인한 경우
+			if (current_ack +1 == recv_ack)
+			{
+				//보내야할 데이터 index를 증가시킨다.
+				++current_ack;
+			} else {
+				//재전송한다
+				goto SEND_DATA;
+			}
+			
+			//다음 while문을 위한 버퍼초기화
 			bzero(buf, BUFFER_SIZE);
+			bzero(head_buf, HEAD_BUFFER_SIZE); 
+			bzero(body_buf, BODY_BUFFER_SIZE);
+			bzero(ack_buf, HEAD_BUFFER_SIZE);
 		}
 		printf("Ok File %s from Client was Sent!\n", file_name);
 	}
 	
-	/*
-	buf[0] = '?'; buf[1] = '\0';
-	//비연결형 서비스에서 데이터전송에 사용하는 함수. 연결형 서비스에서는 send 함수를 사용한다.
-	//첫번쨰 인자는 클라이언트와 서버 사이의 연결설정 과정을 거치지 않은 socket descriptor이다.
-	buf_len = sendto(sock, buf, strlen(buf) + 1, 0, (struct sockaddr *)&server, sizeof(server));
-	
-	if (buf_len < 0) 
-	{
-		printf("sendto error\n");
-		//exit(1);
-		return 1;		
-	}
-	*/
-	/* 소켓을 통해 읽은 데이터는 buf가 가리키는 공간에 저장되며, 데이터 크기가 반환된다. */
-	/*
-	buf_len = recvfrom(sock, buf, 256, 0, (struct sockaddr *)0, (int *)0); //연결형은 recv, 비연결형은 recvfrom을 사용
-	
-	if ( buf_len < 0 )
-	{
-		printf("recvfrom error\n");
-		//exit(1);
-		return 1;				
-	}
-	
-	printf("Time information from server is %s", buf);
-	*/
 	close(sock);
 }
--- C/UDPFile/UDPServer.c
+++ C/UDPFile/UDPServer.c
@@ -5,8 +5,9 @@
 #include<netinet/in.h>
 #include <errno.h>
 
-#define TIME_PORT 8080
-#define BUFFER_SIZE 1024
+#define SERVER_PORT 			8080
+#define HEAD_BUFFER_SIZE 	8
+#define BODY_BUFFER_SIZE 	1024
 
 void error(char* msg) {
 	fprintf(stderr, "ERROR: %s. (errno = %d)\n", msg, errno);
@@ -15,40 +16,40 @@ void error(char* msg) {
 
 int main(int argc, char *argv[]) 
 {
+	const int BUFFER_SIZE = HEAD_BUFFER_SIZE + BODY_BUFFER_SIZE;
+	
 	/* 변수 선언 */
 	int sock;	//생성된 소켓번호(socket descriptor)를 저장할 변수
 	struct sockaddr_in server, client;  //서버 주소정보를 담을 변수들
-	int client_len = sizeof(client); //클라이언트 소켓주소정보의 크기를 담은 변수
-	char buf[BUFFER_SIZE];	//송. 수신데이터를 담을 버퍼변수
-	int buf_len;	//전송, 수신데이터 크기를 담을 변수
-	time_t today;
+	int clientaddr_len;
+	
+	char head_buf[HEAD_BUFFER_SIZE]; 
+	char body_buf[BODY_BUFFER_SIZE];	char buf[BUFFER_SIZE];	//송. 수신데이터를 담을 버퍼변수 (head_buf + body_buf)
+	char ack_buf[HEAD_BUFFER_SIZE]; //acknoledgement를 받기위한 buffer
 	
 	/* 데이터 전송에 사용되  는 소켓을 생성하고, 생성된 소켓번호 descriptor(각 소켓간의 구분자)를 가져온다.*/
 	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1 ) 
 	{
-		printf("socket error\n");
-		//exit(1);
-		return 1;
+		error("socket() Fail");
 	}
 	
 	/* 소켓 주소정보를 담고있는 sockaddr_in sturcture에 서버 호스트에 대한 정보를 저장한다. */
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	server.sin_port = htons(TIME_PORT);
+	server.sin_port = htons(SERVER_PORT);
 	
 	//상대 프로세스와 통신하려면 위에서 생성한 소켓에 주소를 부여해야 한다.
 	//이때 socket descriptor를 이용한다.
 	if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0 ) 
 	{
-		printf("bind error\n");
-		//exit(1);
-		return 1;
+		error("bind() Fail");
 	}
 	
 	while(1) 
-	{
-		/* 클라이언트로부터 파일을 전송받는다. */
+	{		
+		/* 서버에서 처리해야하는 일을 수행하는 Business Logic부분 */
 		char* file_name = "result.pdf";
+		
 		FILE* file = fopen(file_name, "a");
 
 		if(file == NULL)
@@ -57,66 +58,93 @@ int main(int argc, char *argv[])
 		} 
 		else 
 		{
+			//버퍼변수 초기화
 			bzero(buf, BUFFER_SIZE); 
-			int fr_block_sz = 0;
-			int buffer_size = BUFFER_SIZE;
-					
-			while((fr_block_sz = recvfrom(sock, buf, BUFFER_SIZE, 0, (struct sockaddr *)&client, &client_len)) > 0) 
+			bzero(head_buf, HEAD_BUFFER_SIZE); 
+			bzero(body_buf, BODY_BUFFER_SIZE); 
+			bzero(ack_buf, HEAD_BUFFER_SIZE); 
+			
+			int fr_block_sz = 0;	//클라이언트에서 전달받는 buffer data크기
+			clientaddr_len = sizeof(client);//client의 sockaddr의 크기를 저장 
+			int recv_ack = 0, current_ack = 0;//acknoledgement를 저장할 변수.
+			int write_sz; //전달받은 데이터를 파일에 쓸때, 작성된 데이터의 크기를 저장할 변수
+			
+			//모든 데이터를 클라이언트로부터 받는다.
+			while((fr_block_sz = recvfrom(sock, buf, BUFFER_SIZE, 0, (struct sockaddr *)&client, &clientaddr_len)) > 0) 
 			{
-				//printf("buf : %s\n", buf);
-				printf("fr_block_sz : %d\n", fr_block_sz);
-				int write_sz = fwrite(buf, sizeof(char), fr_block_sz, file);
-				if(write_sz < fr_block_sz)
+				//printf("in while\n"); //test
+				
+				//헤더버퍼를 추출
+				for (int i = 0 ; i < HEAD_BUFFER_SIZE ; ++i) 
 				{
-					error("File write failed on server.\n");
+					head_buf[i] = buf[i];
 				}
-				bzero(buf, BUFFER_SIZE);
-						
-				if (fr_block_sz == 0 || fr_block_sz != buffer_size) 
+				
+				//바디버퍼를 추출
+				for (int i = 0 ; i < fr_block_sz-HEAD_BUFFER_SIZE ; ++i) 
 				{
+					body_buf[i] = buf[HEAD_BUFFER_SIZE+i];
+				}
+				
+				//헤더버퍼에서 문자열로 이루어진 숫자를 integer로 변환
+				recv_ack = atoi(head_buf);
+								
+				//보내준 데이터가 현재 받아야하는 데이터순서와 일치할경우
+				if (current_ack == recv_ack)
+				{
+WRITE_DATA_ON_FILE:
+					//데이터를 파일에 쓴다.
+					write_sz = fwrite(body_buf, sizeof(char), fr_block_sz-HEAD_BUFFER_SIZE, file);
+					
+					//써야할 buffer사이즈만큼 작성에 성공했을경우
+					if (write_sz == fr_block_sz-HEAD_BUFFER_SIZE)
+					{
+						//다음번 offset을 증가시킨다.
+						++current_ack;
+						
+					//작성에 실패할 경우
+					} else {
+						goto WRITE_DATA_ON_FILE;
+					}				
+				}
+				//현재 전달받아야하는 offset을 버퍼에 저장한다.
+				sprintf(ack_buf, "%d", current_ack);
+				
+				//offset에 대한 acknoledgement를 클라이언트에게 회신해준다.
+				sendto(sock, ack_buf, HEAD_BUFFER_SIZE, 0, (struct sockaddr *)&client, sizeof(client));
+				
+				//다음 while문을 위한 버퍼초기화
+				bzero(buf, BUFFER_SIZE);
+				bzero(body_buf, BODY_BUFFER_SIZE);
+				bzero(head_buf, HEAD_BUFFER_SIZE);
+				
+				//전달받은 데이터가 전혀없거나,
+				//마지막 데이터를 수신했을경우
+				//질문, break를 하지 않을경우 데이터가 없어지는 현상
+				if (fr_block_sz == 0 || fr_block_sz != BUFFER_SIZE) 
+				{
+					//printf("recv_ack : %d\n", recv_ack);
+					//printf("current_ack : %d\n", current_ack);
 					break;
 				}
 				
-			}
-					
-			if(fr_block_sz < 0)
-			{
-				if (errno == EAGAIN)
+				//데이터 수신에 실패할 경우
+				if(fr_block_sz < 0)
 				{
-					printf("recv() timed out.\n");
-				}
-				else
-				{
-					fprintf(stderr, "recv() failed due to errno = %d\n", errno);
-					exit(1);
-				}
+					if (errno == EAGAIN)
+					{
+						error("recv() timed out");
+					}
+					else
+					{
+						error("recv() failed due to errno = %d");
+					}
+				}	
 			}
+			
 			printf("Ok received from client!\n");
-			fclose(file); 
+			fclose(file);
 		}//else
-
-		
-		
-		///
-		/* 소켓을 통해 읽은 데이터는 buf가 가리키는 공간에 저장되며, 데이터 크기가 반환된다. */
-		//연결형은 recv, 비연결형은 recvfrom을 사용
-		/*
-		buf_len = recvfrom(sock, buf, 256, 0, (struct sockaddr *)&client, &client_len);
-		
-		if ( buf_len < 0 ) {
-			printf("recvfrom error\n");
-			//exit(1);
-			return 1;
-		}
-		
-		printf("Server : Got %s\n", buf);
-		
-		time(&today);
-		strcpy(buf, ctime(&today));
-		
-		//비연결형 서비스에서 데이터전송에 사용하는 함수. 연결형 서비스에서는 send 함수를 사용한다.
-		//첫번쨰 인자는 클라이언트와 서버 사이의 연결설정 과정을 거치지 않은 socket descriptor이다.
-		sendto(sock, buf, strlen(buf) + 1, 0, (struct sockaddr *)&client, client_len);
-		*/
 	}
+	close(sock);
 }
